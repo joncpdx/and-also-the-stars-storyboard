411e427b242fca34db16095b085096ba73fa3eec
joncpdx <jonpdx@gmail.com>
2024-09-07 08:20:05 -0700

    Create index.js

---
+++ index.js
@@ -0,0 +1,11 @@
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import './index.css';
+import StoryBoard from './App';
+
+const root = ReactDOM.createRoot(document.getElementById('root'));
+root.render(
+  <React.StrictMode>
+    <StoryBoard />
+  </React.StrictMode>
+);
